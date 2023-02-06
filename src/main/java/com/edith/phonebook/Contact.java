6fede12eb6969175b9057a5019e5446f45d294ab
M-edith <edithrand505@gmail.com>
2023-02-06 17:02:50 +0100

    getters and setters

---
+++ src/main/java/com/edith/phonebook/Contact.java
@@ -0,0 +1,31 @@
+package com.edith.phonebook;
+
+public class Contact {
+    private String lastName;
+    private String firstName;
+    private String phoneNumber;
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/main/java/com/edith/phonebook/PhoneBook.java
+++ src/main/java/com/edith/phonebook/PhoneBook.java
@@ -5,25 +5,32 @@ import java.util.Scanner;
 public class PhoneBook {
 
 	public static Scanner sc = null;
+
 	public static void main(String[] args) {
-		
+
 		sc = new Scanner(System.in);
 		String userLastName = getUserInput("Entrer votre Nom : ");
 		String userFirstName = getUserInput("Entrer votre Prenom : ");
 		String userPhoneNumber = getUserInput("Entrer votre Numero de telephone : ");
 
+		Contact newContact = new Contact();
+		newContact.setLastName(userLastName);
+		newContact.setFirstName(userFirstName);
+		newContact.setPhoneNumber(userPhoneNumber);
+
 		System.out.println(userLastName);
 		System.out.println(userFirstName);
 		System.out.println(userPhoneNumber);
 
-		System.out.println("Vous avez saisi les informations suivantes { Nom : " + userLastName + " ; Prenom : " +userFirstName+ " ; Numero telephone : " +userPhoneNumber + " }");
+		System.out.println("Vous avez saisi les informations suivantes { Nom : " + userLastName + " ; Prenom : "
+				+ userFirstName + " ; Numero telephone : " + userPhoneNumber + " }");
 		sc.close();
 
 	}
-	public static String getUserInput(String userMessage){
+
+	public static String getUserInput(String userMessage) {
 		System.out.println(userMessage);
-		String userType = sc.nextLine();
-		return userType;
+		return sc.nextLine();
 	}
 
 }
